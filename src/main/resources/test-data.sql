ffb06b429896578582b7e48cd25f2279097bdbd9
M.Born <M.Born@cimsolutions.nl>
2021-10-07 09:09:40 +0200

    Merge met develop

--- src/main/resources/test-data.sql
+++ src/main/resources/test-data.sql
@@ -1,8 +1,3 @@
-INSERT INTO zone (id, title) VALUES (1, 'Utrecht');
-INSERT INTO zone (id, title) VALUES (2, 'Groningen');
-INSERT INTO zone (id, title) VALUES (3, 'Noord-Brabant');
-INSERT INTO zone (id, title) VALUES (4, 'Limburg');
-
 INSERT INTO supplierProduct (id, supplierId, productId, quantity, basePrice, pickUpAddressId, eta)
 VALUES (1, 1, 1, 10, 900, 1, '2021-11-23 15:15:00');
 INSERT INTO product (id, supplierProductId, name, price, sizeId, isArchived)
