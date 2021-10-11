49d5f6f543ae61d1fb4960c8ad6502f66dd321e5
DESKTOP-373G8PH\kelvi <k.van.boekel@cimsolutions.nl>
2021-10-11 10:57:29 +0200

    ZoneDAOTests updated with zone points

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,6 +1,15 @@
 DROP TABLE IF EXISTS Zone;
+DROP TABLE IF EXISTS ZonePoint;
 
 create table zone (
     id int GENERATED ALWAYS AS IDENTITY not null primary key,
     title NVARCHAR2(255)
+);
+
+create table zonePoint(
+    id int GENERATED ALWAYS AS IDENTITY not null primary key,
+    zoneId int,
+    index int,
+    latitude decimal(18,15),
+    longitude decimal(18,15)
 );
--- src/main/resources/test-data.sql
+++ src/main/resources/test-data.sql
@@ -1,4 +1,6 @@
 INSERT INTO zone (id, title) VALUES (1, 'Utrecht')
-INSERT INTO zone (id, title) VALUES (2, 'Groningen')
-INSERT INTO zone (id, title) VALUES (3, 'Noord-Brabant')
-INSERT INTO zone (id, title) VALUES (4, 'Limburg')
+
+INSERT INTO zonePoint (id, zoneId, latitude, longitude) VALUES (1, 1, 5.123, 5.123)
+INSERT INTO zonePoint (id, zoneId, latitude, longitude) VALUES (2, 1, 5.123, 5.123)
+INSERT INTO zonePoint (id, zoneId, latitude, longitude) VALUES (3, 1, 5.123, 5.123)
+INSERT INTO zonePoint (id, zoneId, latitude, longitude) VALUES (4, 1, 5.123, 5.123)
