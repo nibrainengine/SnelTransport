ed36263f11e0d1769df5dbc2c8fde2396a8e5dee
Anietie Okon <A.Okon@cimsolutions.nl>
2021-10-25 15:19:59 +0200

    connecting with python endpoint

---
+++ src/main/java/CIMSOLUTIONS/SnelTransport/PythonEndpoint/PythonEndPoint.java
@@ -0,0 +1,98 @@
+package CIMSOLUTIONS.SnelTransport.PythonEndpoint;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+
+public class PythonEndPoint {
+
+    /**
+     * This method gets the data from python
+     */
+    public void getDataFromPython(){
+        try {
+            URL url = new URL("http://http://127.0.0.1:5000/connect");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Accept", "application/json");
+
+            if (conn.getResponseCode() != 200) {
+                throw new RuntimeException("Failed : HTTP error code : "
+                + conn.getResponseCode());
+            }
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(
+                    (conn.getInputStream())));
+
+            String output;
+            System.out.println("Output from Server .... \n");
+            while ((output = br.readLine()) != null) {
+                System.out.println(output);
+            }
+
+            conn.disconnect();
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     *method for posting the data to python
+     * @param inputData contains arraylist posted to python flask
+     */
+
+    public void postDataToPython(String[] inputData){
+        HttpURLConnection conn = null;
+        DataOutputStream os = null;
+        try{
+            URL url = new URL("http://http://127.0.0.1:5000/connect"); //important to add the trailing slash after add
+            for(String input: inputData){
+                byte[] postData = input.getBytes(StandardCharsets.UTF_8);
+                conn = (HttpURLConnection) url.openConnection();
+                conn.setDoOutput(true);
+                conn.setRequestMethod("POST");
+                conn.setRequestProperty("Content-Type", "application/json");
+                conn.setRequestProperty( "charset", "utf-8");
+                conn.setRequestProperty("Content-Length", Integer.toString(input.length()));
+                os = new DataOutputStream(conn.getOutputStream());
+                os.write(postData);
+                os.flush();
+
+                if (conn.getResponseCode() != 200) {
+                    throw new RuntimeException("Failed : HTTP error code : "
+                            + conn.getResponseCode());
+                }
+
+                BufferedReader br = new BufferedReader(new InputStreamReader(
+                        (conn.getInputStream())));
+
+                String output;
+                System.out.println("Output from Server .... \n");
+                while ((output = br.readLine()) != null) {
+                    System.out.println(output);
+                }
+                conn.disconnect();
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }catch (IOException e){
+            e.printStackTrace();
+        }finally
+        {
+            if(conn != null)
+            {
+                conn.disconnect();
+            }
+        }
+    }
+}
+
+
